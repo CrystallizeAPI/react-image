08042d194f49c38236708571f9feffbe53efc39b
Håkon Krogh <hakonhk@gmail.com>
2020-11-11 09:52:49 +0100

    fix: lint fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
 import React, { FC, HTMLAttributes, FunctionComponent } from 'react';
 
 export interface CrystallizeImageVariant {
-  url: string,
-  width: number,
-  height: number
+  url: string;
+  width: number;
+  height: number;
 }
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   children?: FunctionComponent<any>;
-  src: string,
-  url: string,
-  sizes: string,
-  altText: string,
-  alt: string,
-  variants: CrystallizeImageVariant[]
+  src: string;
+  url: string;
+  sizes: string;
+  altText: string;
+  alt: string;
+  variants: CrystallizeImageVariant[];
 }
 
 function getVariantSrc(variant: CrystallizeImageVariant): string {
-  return `${variant.url} ${variant.width}w`
-};
+  return `${variant.url} ${variant.width}w`;
+}
 
 export const Image: FC<Props> = ({ children, ...restOfAllProps }) => {
   // Regular image
@@ -42,38 +42,41 @@ export const Image: FC<Props> = ({ children, ...restOfAllProps }) => {
     ...rest
   } = restOfAllProps;
 
-  const vars = (variants || []).filter((v) => !!v);
-  const alt = typeof altPassed === "string" ? altPassed : altText;
+  const vars = (variants || []).filter(v => !!v);
+  const alt = typeof altPassed === 'string' ? altPassed : altText;
 
   const hasVariants = vars.length > 0;
 
   // Get the biggest image from the variants
   let biggestImage: CrystallizeImageVariant = vars[0];
   if (hasVariants) {
-    biggestImage = vars
-      .reduce(function (acc: CrystallizeImageVariant, v: CrystallizeImageVariant):CrystallizeImageVariant {
-        if (!acc.width || v.width > acc.width) {
-          return v;
-        }
-        return acc;
-      }, vars[0]);
+    biggestImage = vars.reduce(function(
+      acc: CrystallizeImageVariant,
+      v: CrystallizeImageVariant
+    ): CrystallizeImageVariant {
+      if (!acc.width || v.width > acc.width) {
+        return v;
+      }
+      return acc;
+    },
+    vars[0]);
   }
 
   // Determine srcSet
-  const std = vars.filter((v) => v.url && !v.url.endsWith(".webp"));
-  const webp = vars.filter((v) => v.url && v.url.endsWith(".webp"));
-  const srcSet = std.map(getVariantSrc).join(", ");
-  const srcSetWebp = webp.map(getVariantSrc).join(", ");
+  const std = vars.filter(v => v.url && !v.url.endsWith('.webp'));
+  const webp = vars.filter(v => v.url && v.url.endsWith('.webp'));
+  const srcSet = std.map(getVariantSrc).join(', ');
+  const srcSetWebp = webp.map(getVariantSrc).join(', ');
 
   // Determine the original file extension
-  let originalFileExtension = "jpeg";
+  let originalFileExtension = 'jpeg';
   if (std.length > 0) {
-    const match = std[0].url.match(/\.(?<name>[^\.]+)$/);
+    const match = std[0].url.match(/\.(?<name>[^.]+)$/);
     let originalFileExtension = match?.groups?.name;
 
     // Provide correct mime type for jpg
-    if (originalFileExtension === "jpg") {
-      originalFileExtension = "jpeg";
+    if (originalFileExtension === 'jpg') {
+      originalFileExtension = 'jpeg';
     }
   }
 
@@ -82,8 +85,8 @@ export const Image: FC<Props> = ({ children, ...restOfAllProps }) => {
     src: src || url || (hasVariants ? std[0].url : undefined),
     alt,
     width: biggestImage?.width,
-    height: biggestImage?.height
-  }
+    height: biggestImage?.height,
+  };
 
   if (children) {
     return children({
@@ -110,10 +113,8 @@ export const Image: FC<Props> = ({ children, ...restOfAllProps }) => {
         />
       )}
 
-      <img
-        {...commonProps}
-        {...rest}
-      />
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <img {...commonProps} {...rest} />
     </picture>
   );
 };
--- test/Image.test.tsx
+++ test/Image.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
 
-describe('Thing', () => {
+import { Default as Image } from '../stories/Image.stories';
+
+describe('Image', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Image />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
